084f495406810bfc2a571c1effddcb2925ffdc45
Malcolm Ryan <malcolm.ryan@gmail.com>
2020-10-15 13:17:36 +1100

    Added project

---
+++ COMP2160 Prac Week 11/Assets/Scripts/ControlPuck.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlPuck : MonoBehaviour
+{
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        Vector3 position = MousePosition();
+        transform.position = position;
+    }
+
+    private Vector3 MousePosition()
+    {
+        // use raycasting to turn mouse position into position on the board
+        Plane plane = new Plane(Vector3.up, transform.position);
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        float t;
+        plane.Raycast(ray, out t);
+        return ray.GetPoint(t);
+    }
+}
